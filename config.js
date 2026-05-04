4b3c18b60aaca8bb5717587c3375819edf8942f6
tohirjonov13-blip <tohirjonov.13@gmail.com>
2026-05-04 20:15:06 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Contab — Конфигурация внешних ресурсов
 // Обновляйте ТОЛЬКО этот файл при переразвёртывании Apps Script
 const CONTAB_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbyu2MhnYoEuznqDrO-11ZIOdOJKHf0LlUxdnqYO7J4KP9-zphtkJpCtxojMstDnlULbug/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwL2Zdh-Cp6GhVZWvNoNU3btHRDFIyNl5IbQyYShJ7w1Qy5L71oTVjLjI_S2s43P3BuRQ/exec'
 };
