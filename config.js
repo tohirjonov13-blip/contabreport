7e16343bc057684c0cf2cbd7fe07e0691df5e161
tohirjonov13-blip <tohirjonov.13@gmail.com>
2026-05-04 18:54:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Contab — Конфигурация внешних ресурсов
 // Обновляйте ТОЛЬКО этот файл при переразвёртывании Apps Script
 const CONTAB_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwzUpXozE_Qbn-jABcug61qHaLpFSHpV8GB6HPNLKuseCK8ZR6I-wl5wlFaCTICAk6JVw/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyu2MhnYoEuznqDrO-11ZIOdOJKHf0LlUxdnqYO7J4KP9-zphtkJpCtxojMstDnlULbug/exec'
 };
