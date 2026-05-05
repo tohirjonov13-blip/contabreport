e3dee3f02088548f783528a58fae70a371f63825
tohirjonov13-blip <tohirjonov.13@gmail.com>
2026-05-05 19:27:23 +0500

    Update API URL in configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Contab — Конфигурация внешних ресурсов
 // Обновляйте ТОЛЬКО этот файл при переразвёртывании Apps Script
 const CONTAB_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwL2Zdh-Cp6GhVZWvNoNU3btHRDFIyNl5IbQyYShJ7w1Qy5L71oTVjLjI_S2s43P3BuRQ/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyAy8KgG1hRbc2AYoKKykCaZMEw-zpaY_GpKRZNiHHpZL1uKXcHRv--lXlnGwvLUMFGNg/exec'
 };
